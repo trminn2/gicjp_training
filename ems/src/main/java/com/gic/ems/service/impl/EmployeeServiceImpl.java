8228ebe0991b6c82b4b2a5908023d3ec68921b3a
moepwintphyu <moepwint7phyu@gmail.com>
2019-01-12 12:30:13 +0630

    @spring transaction

--- ems/src/main/java/com/gic/ems/service/impl/EmployeeServiceImpl.java
+++ ems/src/main/java/com/gic/ems/service/impl/EmployeeServiceImpl.java
@@ -1,8 +1,8 @@
 package com.gic.ems.service.impl;
 
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import com.gic.ems.dao.EmployeeDao;
 import com.gic.ems.entity.Employee;
 import com.gic.ems.service.EmployeeService;
