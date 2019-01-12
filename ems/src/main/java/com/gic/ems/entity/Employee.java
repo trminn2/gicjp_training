97dadf3fd5ef04bdebf55722ae850a9555a537bf
moepwintphyu <moepwint7phyu@gmail.com>
2019-01-12 09:45:00 +0630

    employee initial regist

---
+++ ems/src/main/java/com/gic/ems/entity/Employee.java
@@ -0,0 +1,92 @@
+package com.gic.ems.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+	@Id
+	@Column(name = "employeeId")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String employeeId;
+	
+	@Column(name = "firstName", nullable = false)
+	private String firstName;
+	
+	@Column(name = "lastName", nullable = false)
+	private String lastName;
+	
+	@Column(name = "firstNameKana", nullable = false)
+	private String firstNameKana;
+	
+	@Column(name = "lastNameKana", nullable = false)
+	private String lastNameKana;
+	
+	@Column(name = "gender", nullable = false)
+	private String gender;
+	
+	@Column(name = "email", nullable = false)
+	private String email;
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstNameKana() {
+		return firstNameKana;
+	}
+
+	public void setFirstNameKana(String firstNameKana) {
+		this.firstNameKana = firstNameKana;
+	}
+
+	public String getLastNameKana() {
+		return lastNameKana;
+	}
+
+	public void setLastNameKana(String lastNameKana) {
+		this.lastNameKana = lastNameKana;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
