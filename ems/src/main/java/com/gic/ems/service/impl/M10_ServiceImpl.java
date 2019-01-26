ca6c22f9b8c57d7e64daaeac5d231d76c6bbbe50
KYIMINHAN <KYIMINHAN@192.168.100.126>
2019-01-26 17:40:26 +0900

    commit for the spring boot dependencies

--- ems/src/main/java/com/gic/ems/service/impl/M10_ServiceImpl.java
+++ ems/src/main/java/com/gic/ems/service/impl/M10_ServiceImpl.java
@@ -2,6 +2,8 @@ package com.gic.ems.service.impl;
 
 import java.util.Collection;
 
+import org.springframework.stereotype.Service;
+
 import com.gic.ems.service.M10_Service;
 import com.gic.ems.web.model.M10_HakenInfoSearchModel;
 
@@ -11,6 +13,7 @@ import com.gic.ems.web.model.M10_HakenInfoSearchModel;
  * @author KYIMINHAN Jan 26, 2019 <BR>
  *         The Class M10_ServiceImpl.
  */
+@Service
 public class M10_ServiceImpl implements M10_Service {
 
 	/*
