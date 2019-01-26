1ddc9dd14d94209c4985709567b43929e81017fb
thaemyatnoelwin <thaemyatnoe29@gmail.com>
2019-01-26 14:25:37 +0900

    MG005 EmployeeList

--- ems/src/main/java/com/gic/ems/dao/EmployeeDao.java
+++ ems/src/main/java/com/gic/ems/dao/EmployeeDao.java
@@ -1,10 +1,17 @@
 package com.gic.ems.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.gic.ems.entity.Employee;
 
 @Repository
 public interface EmployeeDao extends JpaRepository<Employee, Long> {
+	
+	//employee list searching Thae 
+	@Query(value = "SELECT * FROM employee LEFT JOIN empgroup ON employee.empGroup_id = empgroup.id ", nativeQuery = true)
+	List<Employee> findAll();
 	
 }
