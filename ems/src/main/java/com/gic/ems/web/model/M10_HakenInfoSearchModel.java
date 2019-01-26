30ab568952039c3eaf2af1d9b977c38737c829f6
KYIMINHAN <KYIMINHAN@192.168.100.126>
2019-01-26 18:35:01 +0900

    commit for the M10 Model properties Configurations

--- ems/src/main/java/com/gic/ems/web/model/M10_HakenInfoSearchModel.java
+++ ems/src/main/java/com/gic/ems/web/model/M10_HakenInfoSearchModel.java
@@ -1,14 +1,64 @@
 package com.gic.ems.web.model;
 
+import java.time.LocalDate;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * The Class M10_HakenInfoSearchModel.
  *
  * @author KYIMINHAN Jan 26, 2019 <BR>
  *         The Class M10_HakenInfoSearchModel.
  */
+@Getter
+@Setter
+
+/**
+ * Instantiates a new m 10 haken info search model.
+ */
+@NoArgsConstructor
 public class M10_HakenInfoSearchModel extends BaseModel {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
 
+	/** The company name. */
+	private String companyName;
+
+	/** The destination name. */
+	private String destinationName;
+
+	/** The name. */
+	private String name;
+
+	/** The start date. */
+	private LocalDate startDate;
+
+	/** The end date. */
+	private LocalDate endDate;
+
+	/**
+	 * Instantiates a new m 10 haken info search model.
+	 *
+	 * @param id              the id
+	 * @param uuid            the uuid
+	 * @param companyName     the company name
+	 * @param destinationName the destination name
+	 * @param name            the name
+	 * @param startDate       the start date
+	 * @param endDate         the end date
+	 */
+	@Builder
+	public M10_HakenInfoSearchModel(Long id, String uuid, String companyName, String destinationName, String name,
+			LocalDate startDate, LocalDate endDate) {
+		super(id, uuid);
+		this.companyName = companyName;
+		this.destinationName = destinationName;
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
 }
