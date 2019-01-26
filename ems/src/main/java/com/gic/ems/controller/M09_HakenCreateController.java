d7addf362517cf369e1820a30c7504710e2cb528
thetthetkhine <thetthetkhine@gicjp.biz>
2019-01-26 20:01:08 +0900

    M09 for save

--- ems/src/main/java/com/gic/ems/controller/M09_HakenCreateController.java
+++ ems/src/main/java/com/gic/ems/controller/M09_HakenCreateController.java
@@ -5,6 +5,8 @@
  */
 
 package com.gic.ems.controller;
+import java.util.Locale;
+
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,23 +18,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.gic.ems.common.constant.ControllerConstant;
 import com.gic.ems.service.M09_Service;
 import com.gic.ems.web.model.M09_HakenCreateModel;
+import com.gic.ems.web.model.M14_EmpHakenModel;
 
 @Controller
 public class M09_HakenCreateController {
 
 	@Autowired
-	M09_Service service;
+	private M09_Service hakenservice;
 	
-	@GetMapping(value = "/MG09")
-	public String hakenCreate(Model model) {
-		return "M09-haken-create";
+	@GetMapping(value = "/createCompanyHaken")
+	public String init(Model model, Locale locale) {
+		model.addAttribute(ControllerConstant.M09_MODEL, M09_HakenCreateModel.builder().build());
+		return ControllerConstant.M09_HAKEN_CREATE;
 	}
 	
 	@PostMapping("/createCompanyHaken")
-	public String createCompanyHaken(@Valid @ModelAttribute M09_HakenCreateModel hakenModel,
-			BindingResult bindingResult, Model model) {
-
-		
+	public String createCompanyHaken(Model model, Locale locale) {
 		return ControllerConstant.M09_HAKEN_CREATE;
 	}
 	
