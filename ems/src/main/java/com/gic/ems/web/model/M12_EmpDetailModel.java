d26af22150a1a9ee64e6039258f94db070346ee2
KYIMINHAN <KYIMINHAN@192.168.100.126>
2019-01-26 20:08:36 +0900

    commit for the M12 Model sample

--- ems/src/main/java/com/gic/ems/web/model/M12_EmpDetailModel.java
+++ ems/src/main/java/com/gic/ems/web/model/M12_EmpDetailModel.java
@@ -1,5 +1,27 @@
 package com.gic.ems.web.model;
 
-public class M12_EmpDetailModel {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+/**
+ * The Class M12_EmpDetailModel.
+ *
+ * @author KYIMINHAN Jan 26, 2019 <BR>
+ *         The Class M12_EmpDetailModel.
+ */
+@Getter
+@Setter
+
+/**
+ * Instantiates a new m 12 emp detail model.
+ */
+@NoArgsConstructor
+public class M12_EmpDetailModel extends BaseModel {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+	/** The emp info create model. */
+	private M07_EmpInfoCreateModel empInfoCreateModel;
 }
