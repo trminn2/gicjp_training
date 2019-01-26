9f20bb56a94cd5f8db76660e899596177733aafa
thaemyatnoelwin <thaemyatnoe29@gmail.com>
2019-01-26 18:04:35 +0900

    MG13 CustomerRegister

--- ems/src/main/java/com/gic/ems/controller/M13_CustCreateController.java
+++ ems/src/main/java/com/gic/ems/controller/M13_CustCreateController.java
@@ -1,18 +1,16 @@
 package com.gic.ems.controller;
 
+
 import java.util.Locale;
 
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.gic.ems.common.constant.ControllerConstant;
 import com.gic.ems.service.M13_Service;
 import com.gic.ems.web.model.M13_CustCreateModel;
@@ -26,25 +24,19 @@ public class M13_CustCreateController {
 	
 	@Autowired
 	M13_Service service;
-	@Autowired(required=false)
-	private MessageSource messageSource;
 	
 	@GetMapping("/cust-register")
-	public String init(Model model) {
-		String msg04 = messageSource.getMessage("msg04", null, Locale.JAPAN);
-		model.addAttribute("message", msg04);
-		model.addAttribute("m04Model", new M13_CustCreateModel());
+	public String init(Model model,Locale locale) {
+		model.addAttribute(ControllerConstant.M13_MODEL, M13_CustCreateModel.builder().build());
 		return ControllerConstant.M13_CUSTOMER_REGISTER;
 	}
 	
-	@PostMapping("/createCustomer")
+	@PostMapping("/create-customer")
 	public String createCustomer(@Valid @ModelAttribute M13_CustCreateModel customerModel,
 			BindingResult bindingResult, Model model) {
 		if(bindingResult.hasErrors()) {
-			String msg04 = messageSource.getMessage("msg04", null, Locale.JAPAN);
-			model.addAttribute("message", msg04);
 			model.addAttribute("companyName", customerModel.getCompanyName());
-			model.addAttribute("m13Model", customerModel);
+			model.addAttribute(ControllerConstant.M13_MODEL, customerModel);
 		} else {
 			service.save(customerModel);
 		}
