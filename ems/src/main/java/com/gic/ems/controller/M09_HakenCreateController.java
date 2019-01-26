b9c771d6d4e287094874de023c653e93bb3bbed5
thetthetkhine <thetthetkhine@gicjp.biz>
2019-01-26 20:26:43 +0900

    M09-save function

--- ems/src/main/java/com/gic/ems/controller/M09_HakenCreateController.java
+++ ems/src/main/java/com/gic/ems/controller/M09_HakenCreateController.java
@@ -18,22 +18,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.gic.ems.common.constant.ControllerConstant;
 import com.gic.ems.service.M09_Service;
 import com.gic.ems.web.model.M09_HakenCreateModel;
+import com.gic.ems.web.model.M13_CustCreateModel;
 import com.gic.ems.web.model.M14_EmpHakenModel;
 
 @Controller
 public class M09_HakenCreateController {
 
 	@Autowired
-	private M09_Service hakenservice;
+	private M09_Service service;
 	
-	@GetMapping(value = "/createCompanyHaken")
+	@GetMapping(value = "/create-CompanyHaken")
 	public String init(Model model, Locale locale) {
 		model.addAttribute(ControllerConstant.M09_MODEL, M09_HakenCreateModel.builder().build());
 		return ControllerConstant.M09_HAKEN_CREATE;
 	}
 	
-	@PostMapping("/createCompanyHaken")
-	public String createCompanyHaken(Model model, Locale locale) {
+	@PostMapping("/create-CompanyHaken")
+	public String createCompanyHaken(@Valid @ModelAttribute M09_HakenCreateModel hakenModel,
+			BindingResult bindingResult, Model model) {
+		service.save(hakenModel);
 		return ControllerConstant.M09_HAKEN_CREATE;
 	}
 	
--- ems/src/main/java/com/gic/ems/service/impl/M09_ServiceImpl.java
+++ ems/src/main/java/com/gic/ems/service/impl/M09_ServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.gic.ems.dao.HakenDao;
+import com.gic.ems.entity.Company;
 import com.gic.ems.entity.DispatchDepartment;
 import com.gic.ems.service.M09_Service;
 import com.gic.ems.web.model.M09_HakenCreateModel;
@@ -18,6 +19,18 @@ public class M09_ServiceImpl implements M09_Service{
 	@Transactional(noRollbackFor = Exception.class)
 	public void save(M09_HakenCreateModel m14Model) {
 		// TODO Auto-generated method stub
-		this.hakenDao.save(DispatchDepartment.builder().build());
+		//this.hakenDao.save(DispatchDepartment.builder().build());
+		DispatchDepartment dispatchDept = DispatchDepartment.builder().
+				dispatchDepartmentId(m14Model.getDispatchDepartmentId()).
+				dispatchDepartmentName(m14Model.getName()).
+				postalCode(m14Model.getPostalCode()).
+				address(m14Model.getAddress()).
+				contractPersonFirstName(m14Model.getContractPersonFirstName()).
+				contractPersonFirstNameKana(m14Model.getContractPersonFirstNameKana()).
+				contractPersonLastName(m14Model.getContractPersonLastName()).
+				contractPersonLastNameKana(m14Model.getContractPersonLastNameKana()).
+				contractPhone(m14Model.getContractPhone()).
+				contractEmail(m14Model.getContractEmail()).build();
+		this.hakenDao.save(dispatchDept);
 	}
 }
