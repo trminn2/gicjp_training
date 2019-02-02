bc0f643791c87b71c458853cc799df1e8245f930
thaemyatnoelwin <thaemyatnoe29@gmail.com>
2019-02-02 11:41:03 +0900

    Modified

--- ems/src/main/java/com/gic/ems/service/M15_Service.java
+++ ems/src/main/java/com/gic/ems/service/M15_Service.java
@@ -2,8 +2,14 @@ package com.gic.ems.service;
 
 import java.util.Collection;
 
-import com.gic.ems.web.model.M15_CustomerListModel;
 
+import com.gic.ems.web.model.M15_CustomerListModel;
+/**
+ * The Interface M15_Service.
+ *
+ * @author ThaeMyatNoeLwin Jan 27, 2019 <BR>
+ *         The Interface M05_Service.
+ */
 public interface M15_Service {
 	public Collection<M15_CustomerListModel> findAll(M15_CustomerListModel searchModel);
 }
