60598b3664735fc3d655ab6b59cccdd5c5ef38e8
KYIMINHAN <KYIMINHAN@192.168.100.111>
2019-02-02 17:03:42 +0900

    commit for the Generate code

--- ems/src/main/java/com/gic/ems/common/utility/CodeUtility.java
+++ ems/src/main/java/com/gic/ems/common/utility/CodeUtility.java
@@ -1,20 +1,83 @@
 package com.gic.ems.common.utility;
 
+import io.micrometer.core.lang.NonNull;
+
+/**
+ * The Class CodeUtility.</BR>
+ *
+ * @author KYIMINHAN </BR>
+ * @version 1.0 </BR>
+ * @since Feb 2, 2019 </BR>
+ *        ems system </BR>
+ *        com.gic.ems.common.utility </BR>
+ *        CodeUtility.java </BR>
+ */
 public class CodeUtility {
 
-	public static CodeUtility getInstance() {
-		// TODO Auto-generated method stub
-		return null;
+	/** The Constant COMPANY_CODE. */
+	private static final String COMPANY_CODE = "COM";
+
+	/** The Constant EMPLOYEE_CODE. */
+	private static final String EMPLOYEE_CODE = "EMP";
+
+	/** The Constant CODE_0. */
+	private static final String CODE_0 = "0";
+
+	/** The Constant CODE_00. */
+	private static final String CODE_00 = "00";
+
+	/** The Constant CODE_000. */
+	private static final String CODE_000 = "000";
+
+	/** The Constant CODE_0000. */
+	private static final String CODE_0000 = "0000";
+
+	/** The code utility. */
+	private volatile static CodeUtility codeUtility;
+
+	/**
+	 * Gets the single instance of CodeUtility.
+	 *
+	 * @return CodeUtility
+	 */
+	public static final CodeUtility getInstance() {
+		if (null == codeUtility) {
+			synchronized (DateUtility.class) {
+				codeUtility = new CodeUtility();
+			}
+		}
+		return codeUtility;
 	}
 
-	public String generateCompanyCode() {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Generate company code.
+	 *
+	 * @param companyId the company id
+	 * @return String
+	 */
+	public String generateCompanyCode(@NonNull final Long companyId) {
+		return new StringBuilder(COMPANY_CODE).append(this.getCode(companyId)).toString();
 	}
 
-	public String generateEmployeeCode() {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * Generate employee code.
+	 *
+	 * @param employeeId the employee id
+	 * @return String
+	 */
+	public String generateEmployeeCode(@NonNull final Long employeeId) {
+		return new StringBuilder(EMPLOYEE_CODE).append(this.getCode(employeeId)).toString();
 	}
 
+	/**
+	 * Gets the code.
+	 *
+	 * @param companyId the company id
+	 * @return String
+	 */
+	private String getCode(@NonNull final Long companyId) {
+		int id = companyId.intValue();
+		return (id < 10) ? CODE_0000
+				: (id < 100) ? CODE_000 : (id < 1000) ? CODE_00 : (id < 10000) ? CODE_0 : companyId.toString();
+	}
 }
