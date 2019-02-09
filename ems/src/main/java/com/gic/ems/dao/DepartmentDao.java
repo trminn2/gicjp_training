2f556fc7b8f030c1e77a06ab3b288561ec86a601
thetthetkhine <thetthetkhine@gicjp.biz>
2019-02-09 14:38:39 +0900

    Department Dao

--- ems/src/main/java/com/gic/ems/dao/DepartmentDao.java
+++ ems/src/main/java/com/gic/ems/dao/DepartmentDao.java
@@ -1,8 +1,11 @@
 package com.gic.ems.dao;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.gic.ems.common.type.DeleteFlag;
 import com.gic.ems.entity.Department;
 
 /**
@@ -14,4 +17,11 @@ import com.gic.ems.entity.Department;
 @Repository
 public interface DepartmentDao extends JpaRepository<Department, Long> {
 
+	/**
+	 * Find by delete flag.
+	 *
+	 * @param active the active
+	 * @return Collection
+	 */
+	Collection<Department> findAllByDeleteFlag(DeleteFlag active);
 }
